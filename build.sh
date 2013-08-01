52ae53b218cf008bd8b3d17f8d8ebdf20055f6ea
Brett Terpstra <me@brettterpstra.com>
2013-07-31 20:14:30 -0500

    Poor man's versioning

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
+version=$(cat VERSION|tr -d "\n")
+newversion=$(echo "$version + .1"|bc)
 for file in *.popclipext; do zip ${file}z $file/*; done
-zip BrettsPopclipExtensions$1.zip *.popclipextz README.md
+zip BrettsPopclipExtensions$newversion.zip *.popclipextz README.md
 rm *.popclipextz
+echo $newversion > VERSION
