2adeec73b34f56949628f67ebd282cba124bf609
Brett Terpstra <me@brettterpstra.com>
2013-08-29 10:14:56 -0500

    Option modifier to concatenate strings with no extra whitespace.

--- CopyPlus.popclipext/README.md
+++ CopyPlus.popclipext/README.md
@@ -3,4 +3,6 @@ CopyPLUS
 
 PopClip extension to append the selection to the current contents in the clipboard.
 
-Hold ⌘ when clicking to add a line break between entries.
+Hold Command (⌘) when clicking to add an extra line break between entries.
+
+Hold Option (⌥) to append with no extra whitespace.
--- CopyPlus.popclipext/copyplus.sh
+++ CopyPlus.popclipext/copyplus.sh
@@ -4,14 +4,17 @@ clipboard=`__CF_USER_TEXT_ENCODING=$UID:0x8000100:0x8000100 pbpaste`
 
 # Check for command key
 if [[ $POPCLIP_MODIFIER_FLAGS == 1048576 ]]; then
-	__CF_USER_TEXT_ENCODING=$UID:0x8000100:0x8000100 pbcopy <<EOF
+	__CF_USER_TEXT_ENCODING=$UID:0x8000100:0x8000100 pbcopy <<EOS
 $clipboard
 
 $POPCLIP_TEXT
-EOF
+EOS
+# check for option key, append with no spaces or line break
+elif [[ $POPCLIP_MODIFIER_FLAGS == 524288 ]]; then
+	__CF_USER_TEXT_ENCODING=$UID:0x8000100:0x8000100 pbcopy << "$clipboard$POPCLIP_TEXT"
 else
-	__CF_USER_TEXT_ENCODING=$UID:0x8000100:0x8000100 pbcopy <<EOF
+	__CF_USER_TEXT_ENCODING=$UID:0x8000100:0x8000100 pbcopy <<EOS
 $clipboard
 $POPCLIP_TEXT
-EOF
+EOS
 fi
