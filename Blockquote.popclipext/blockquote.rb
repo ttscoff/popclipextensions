fbaeb32a68c7097a56fc3d4b112c449ce67843a7
Brett Terpstra <me@brettterpstra.com>
2013-08-29 08:33:41 -0500

    Block quote improvements
    Handle line breaks better
    add command key to decrease quote level
    add command-option key to remove all quoting
    don't quote reference link definitions

--- Blockquote.popclipext/blockquote.rb
+++ Blockquote.popclipext/blockquote.rb
@@ -2,12 +2,30 @@
 
 input = ENV['POPCLIP_TEXT']
 output = ""
-input.each do |line|
-	quote = ">"
-	tabs = line.scan(/(\t|\s{4})/)
-	tabs.each {|m|
-		quote += ">"
-	} unless tabs.nil?
-	output += line =~ /^\s*$/ ? "\n" : "#{quote} #{line.strip}\n"
+
+case ENV['POPCLIP_MODIFIER_FLAGS'].to_i
+when 1048576 # Command (remove one level of blockquoting)
+	input.each do |line|
+		output += line.sub(/^>\s*/,'').strip + "\n"
+	end
+when 1572864 # Option-Command (remove all blockquoting)
+	input.each do |line|
+		output += line.gsub(/^(\s*>)*\s*/,'').strip + "\n"
+	end
+else # Increase quote level by one
+	input.each do |line|
+		quote = ">"
+		tabs = line.scan(/(\t|\s{4})/)
+		tabs.each {|m|
+			quote += " >"
+		} unless tabs.nil?
+
+		# don't quote reference definitions
+		unless line =~ /^\s*\[.*?\]: .*/
+			output += line =~ /^\s*$/ ? "#{quote}\n" : "#{quote} #{line.strip}\n"
+		else
+			output += line.chomp + "\n"
+		end
+	end
 end
-print output.sub(/\n$/s,'')
+puts output #.sub(/\n$/s,'')
